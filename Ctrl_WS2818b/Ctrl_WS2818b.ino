38425dab2ab910f12fc52e717bd06fe3b2a45dce
yu124789 <847771987@qq.com>
2017-12-05 21:28:38 +0800

    更新Ctrl_WS2818b.ino。
    暂时还是不能用...好像..

--- Ctrl_WS2818b/Ctrl_WS2818b.ino
+++ Ctrl_WS2818b/Ctrl_WS2818b.ino
@@ -2,53 +2,61 @@
 #include <NeoPixelBrightnessBus.h>
 #include <NeoPixelBus.h>
 
+#define ON 1
+#define OFF 0
+#define DEBUG ON
+
 //定义LED灯串的基本属性
-uint16_t PixelCount=12; //定义控制的个数
-uint8_t pixelPin=7;     //定义引脚
-NeoPixelBus<NeoGrbFeature,Neo800KbpsMethod> strip(PixelCount,pixelPin); //定义实例
+uint16_t pixelCount = 12; //定义控制的个数
+uint8_t pixelPin = 7;   //定义引脚
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(pixelCount, pixelPin); //定义实例
 HslColor lampHslColor;
 
 struct Cmd {
   char head =  ' ';
-  int num = 0;
-}cmd;
+  float num = 0;
+} cmd;
 
 bool isOpen = false;
 
 void setup() {
- Serial.begin(38400);
- strip.Begin();
- strip.Show();
-
+  Serial.begin(38400);
+  strip.Begin();
+  strip.Show();
 }
 
-void setOFF(){
-   for(int i=0;i<PixelCount;i++){
-        strip.SetPixelColor(i,RgbColor(0));                    
-      }
-    strip.Show();
+void setOFF() {
+  for (int i = 0; i < pixelCount; i++) {
+    strip.SetPixelColor(i, RgbColor(0));
+  }
+  strip.Show();
 }
 
 void loop() {
-  if (Serial.available()){
+  if (Serial.available()) {
     cmd.head = Serial.read();
-    cmd.num = Serial.parseInt();
-    switch (cmd.head){
-      case 'H':lampHslColor.H = cmd.num/360.0f;break;
-      case 'S':lampHslColor.S = cmd.num/100.0f;break;
-      case 'L':lampHslColor.L = cmd.num/100.0f;break;
-      case 'P':isOpen = !isOpen;
+    cmd.num = Serial.parseFloat();
+    switch (cmd.head) {
+      case 'H': lampHslColor.H = cmd.num; break;
+      case 'S': lampHslColor.S = cmd.num; break;
+      case 'L': lampHslColor.L = cmd.num; break;
+      case 'P': isOpen = !isOpen;break;
+      case 'C': isOpen = false;break;
     }
-    if (isOpen){
-      for(int i=0;i<PixelCount;i++){
-        strip.SetPixelColor(i,lampHslColor);
+    if (isOpen) {
+      for (int i = 0; i < pixelCount; i++) {
+        strip.SetPixelColor(i, lampHslColor);
       }
       strip.Show();
-    }else{
+    } else {
       setOFF();
     }
-  }else{
-    setOFF();
-  }
-
+#if DEBUG
+Serial.print("head:");Serial.println(cmd.head);
+Serial.print("num:");Serial.println(cmd.num);
+Serial.print("isOpen:");Serial.println(isOpen);
+Serial.print("pixelCount:");Serial.println(pixelCount);
+Serial.println("---------");
+#endif
+  } 
 }
