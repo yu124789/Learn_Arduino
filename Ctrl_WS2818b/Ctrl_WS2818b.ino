7f66906568f357686c3c7a1b8eb8ecf05676bcf8
yu124789 <847771987@qq.com>
2017-12-06 11:35:35 +0800

    改用Adafruit_NeoPixel库。
    能亮了…

--- Ctrl_WS2818b/Ctrl_WS2818b.ino
+++ Ctrl_WS2818b/Ctrl_WS2818b.ino
@@ -1,53 +1,55 @@
-#include <NeoPixelAnimator.h>
-#include <NeoPixelBrightnessBus.h>
-#include <NeoPixelBus.h>
+
+#include <Adafruit_NeoPixel.h>
 
 #define ON 1
 #define OFF 0
 #define DEBUG ON
 
+int r,g,b;
+
 //定义LED灯串的基本属性
-uint16_t pixelCount = 12; //定义控制的个数
-uint8_t pixelPin = 7;   //定义引脚
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(pixelCount, pixelPin); //定义实例
-HslColor lampHslColor;
+uint16_t pixelCount = 9; //定义控制的个数
+uint8_t pixelPin = 6;   //定义引脚
+Adafruit_NeoPixel strip(pixelCount, pixelPin, NEO_RGB + NEO_KHZ800); //定义实例
+uint32_t color = strip.Color(0, 0, 0);
 
 struct Cmd {
   char head =  ' ';
-  float num = 0;
+  int num = 0;
 } cmd;
 
 bool isOpen = false;
 
 void setup() {
+  r = g = b = 0;
   Serial.begin(38400);
-  strip.Begin();
-  strip.Show();
+  strip.begin();
+  strip.show();
 }
 
 void setOFF() {
   for (int i = 0; i < pixelCount; i++) {
-    strip.SetPixelColor(i, RgbColor(0));
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
-  strip.Show();
+  strip.show();
 }
 
 void loop() {
   if (Serial.available()) {
     cmd.head = Serial.read();
-    cmd.num = Serial.parseFloat();
+    cmd.num = (int)Serial.parseFloat();
     switch (cmd.head) {
-      case 'H': lampHslColor.H = cmd.num; break;
-      case 'S': lampHslColor.S = cmd.num; break;
-      case 'L': lampHslColor.L = cmd.num; break;
+      case 'R':  r = cmd.num; break;
+      case 'G':  g = cmd.num; break;
+      case 'B':  b = cmd.num; break;
       case 'P': isOpen = !isOpen;break;
       case 'C': isOpen = false;break;
     }
     if (isOpen) {
       for (int i = 0; i < pixelCount; i++) {
-        strip.SetPixelColor(i, lampHslColor);
+        strip.setPixelColor(i, strip.Color(r, g, b));
       }
-      strip.Show();
+      strip.show();
     } else {
       setOFF();
     }
