85caf48991626a634dbf22ebbd1947910dca35b1
yu124789 <847771987@qq.com>
2017-12-14 09:44:38 +0800

    倒车雷达。
    http://zhongbest.com/2017/12/08/%E5%9F%BA%E4%BA%8Earduino%E7%9A%84%E5%80%92%E8%BD%A6%E9%9B%B7%E8%BE%BE%E7%B3%BB%E7%BB%9F/

---
+++ daocheleida/daocheleida.ino
@@ -0,0 +1,51 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13, 12, 7, 6, 5, 4);
+
+const int
+TONE = 8,
+R = 11,
+G = 10,
+B = 9,
+TRIG = 2,
+ECHO = 3;
+
+double dis = 0;
+
+void disDis(double dis){
+  lcd.clear();
+  lcd.home();
+  lcd.print("distance");
+  lcd.setCursor(0, 1);
+  lcd.print(dis);
+  lcd.print("cm");
+}
+
+void setRGB(int r, int g, int b){
+  analogWrite(R, r);
+  analogWrite(G, g);
+  analogWrite(B, b);
+}
+
+double readDis(int EPin, int TPin){
+  digitalWrite(TPin, LOW);delayMicroseconds(2);
+  digitalWrite(TPin, HIGH);delayMicroseconds(10);
+  digitalWrite(TPin, LOW);
+  return pulseIn(EPin, HIGH)/58.0;
+}
+
+void setup() {
+  for (int i = 8; i < 12;i++) pinMode(i, OUTPUT);
+  pinMode(ECHO, INPUT);pinMode(TRIG, OUTPUT);
+  noTone(TONE);
+  lcd.begin(16, 2);
+  disDis(0);
+  setRGB(255, 0, 0);delay(1000);
+  setRGB(0, 255, 0);delay(1000);
+  setRGB(0, 0, 255);delay(1000);
+  setRGB(0, 0, 0);delay(1000);
+}
+
+void loop() {
+  disDis(dis = readDis(ECHO, TRIG));
+}
