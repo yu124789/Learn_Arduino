473a497715da484a6464b3851bfecf434e7a1948
yu124789 <847771987@qq.com>
2017-12-14 12:50:19 +0800

    倒车雷达的例子。

--- daocheleida/daocheleida.ino
+++ daocheleida/daocheleida.ino
@@ -12,6 +12,7 @@ ECHO = 3;
 
 double dis = 0;
 
+/* display distance. */
 void disDis(double dis){
   lcd.clear();
   lcd.home();
@@ -27,25 +28,56 @@ void setRGB(int r, int g, int b){
   analogWrite(B, b);
 }
 
+/* read distance. */
 double readDis(int EPin, int TPin){
   digitalWrite(TPin, LOW);delayMicroseconds(2);
   digitalWrite(TPin, HIGH);delayMicroseconds(10);
   digitalWrite(TPin, LOW);
-  return pulseIn(EPin, HIGH)/58.0;
+  return pulseIn(EPin, HIGH)/58.0;//cm
+}
+
+void buzzing(int hz, int interval){
+  static int bc = 0;//buzzing count.
+  if (++bc >= interval) {
+    bc = 0;
+    tone(TONE, hz);
+  }else if (bc >= interval/2) noTone(TONE);
 }
 
 void setup() {
-  for (int i = 8; i < 12;i++) pinMode(i, OUTPUT);
-  pinMode(ECHO, INPUT);pinMode(TRIG, OUTPUT);
+  for (int i = 8; i < 12;i++) pinMode(i, OUTPUT);//rgb+tone
+  pinMode(ECHO, INPUT);pinMode(TRIG, OUTPUT);//sr40
+  /* initialize. */
   noTone(TONE);
   lcd.begin(16, 2);
+  /* test. */
   disDis(0);
-  setRGB(255, 0, 0);delay(1000);
-  setRGB(0, 255, 0);delay(1000);
-  setRGB(0, 0, 255);delay(1000);
-  setRGB(0, 0, 0);delay(1000);
+  setRGB(255, 0, 0);delay(500);
+  setRGB(0, 255, 0);delay(500);
+  setRGB(0, 0, 255);delay(500);
+  setRGB(0, 0, 0);
 }
 
+int c = 0;//count.
 void loop() {
-  disDis(dis = readDis(ECHO, TRIG));
+  delay(1);
+  if (1000 <= c++){
+    c = 0;
+    disDis(dis = readDis(ECHO, TRIG));//read distance and display it to lcd.
+  }
+  if (12 >= dis){
+    setRGB(255, 0, 0);//red
+    buzzing(900, 300);
+  }else if (15 >= dis){
+    setRGB(255, 255, 0);//yellow
+    buzzing(800, 400);
+  }else if (18 >= dis){
+    setRGB(0, 255, 0);//green
+    buzzing(700, 500);
+  }else{
+    /* close. */
+    setRGB(0, 0, 0);
+    noTone(TONE);
+  }
 }
+
---
+++ daocheleida/links.md
@@ -0,0 +1,3 @@
+[基于Arduino的倒车雷达系统](http://zhongbest.com/2017/12/08/%E5%9F%BA%E4%BA%8Earduino%E7%9A%84%E5%80%92%E8%BD%A6%E9%9B%B7%E8%BE%BE%E7%B3%BB%E7%BB%9F/)
+[超声波模块](http://zhongbest.com/2016/09/01/%e8%b6%85%e5%a3%b0%e6%b3%a2%e6%a8%a1%e5%9d%97/)
+[有源无源蜂鸣器](http://zhongbest.com/2016/09/01/%E6%9C%89%E6%BA%90%E6%97%A0%E6%BA%90%E8%9C%82%E9%B8%A3%E5%99%A8/)
